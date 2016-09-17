da0acc3a674fa46f49dec741a99df24fa7ebbe1f
Saurabh Nayak <saurabhnayak007@gmail.com>
2016-09-17 12:23:29 -0500

    Create longest_substring_c.py

---
+++ longest_substring_c.py
@@ -0,0 +1,37 @@
+#Given a string, find the length of the longest substring without repeating characters
+
+#Examples:
+
+#Given "abcabcbb", the answer is "abc", which the length is 3.
+
+#Given "bbbbb", the answer is "b", with the length of 1.
+
+#Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+#https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+str = "xxyzzqw"
+count = 0
+j = 1
+l = []
+d = {}
+i = 0
+for i in range (len(str)):
+    if str[i] not in d:
+        print ("1 "+str[i])
+        d[str[i]] = 1
+        count += 1
+
+    else:
+        print(d)
+        print ("2 " + str[i])
+        l.append((len(str[j-1:i]), str[j-1:i]))
+        print (l)
+        j=i+1
+        count = 1
+        d.clear()
+        d[str[i]]= 1
+l.append((len(str[j-1:len(str)]), str[j-1:len(str)]))
+
+l.sort()
+print(l[len(l)-1])
