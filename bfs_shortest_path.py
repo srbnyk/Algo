93e8bd518e3bcaade24499cbd721c48a3eb233f4
Saurabh Nayak <saurabhnayak007@gmail.com>
2016-09-21 15:52:18 -0500

    Update bfs_shortest_path.py

--- bfs_shortest_path.py
+++ bfs_shortest_path.py
@@ -1,31 +1,23 @@
-#https://www.hackerrank.com/challenges/bfsshortreach
-#Consider an undirected graph consisting of  nodes where each node is labeled from  to  and the edge between any two nodes is always of length . We define node  to be the starting position for a BFS.
-#Given  queries in the form of a graph and some starting node, , perform each query by calculating the shortest distance from starting node  to all the other nodes in the graph. Then print a single line of  space-separated integers listing node 's shortest distance to each of the  other nodes (ordered sequentially by node number); if  is disconnected from a node, print  as the distance to that node.
-
-def bfs_shortest(graph,initial,final):
-    if not graph[final]:
-        return -1
+def bfs_shortest(graph,initial,distance):
     visited = set()
     stack = [initial]
-    path = 0
+    distance[initial-1] = 0
     while stack:
         item = stack.pop()
         if item not in visited:
             visited.add(item)
             l = [x for x in graph[item] if x not in visited if x not in stack]
-            if final in l:
-                path += 6
-                return path
-            else:
-                path += 6
             stack = l+stack
-    return -1
+            for elements in l:
+                if distance[elements-1] == -1:
+                    distance[elements-1] = distance[item-1]+6
+    return distance
 
 
 for i in range(int(input())):
     l = [int(x) for x in input().split()]
     nodes, edges = l[0], l[1]
-    #print(nodes)
+    distance = [-1]*nodes
     d = {}
     for i in range (1, nodes+1):
         d[i] = []
@@ -42,9 +34,7 @@ for i in range(int(input())):
     #print(d)
     initial = int(input())
     t=[]
-    for i in range(1,nodes+1):
-        if i != initial:
-            t.append(bfs_shortest(d,initial,i))
-    
-    s = " ".join([str(x) for x in t])
-    print(s)
+    distance= bfs_shortest(d,initial,distance)
+    #print (distance)
+    del distance[initial-1]
+    print (" ".join([str(x) for x in distance]))
